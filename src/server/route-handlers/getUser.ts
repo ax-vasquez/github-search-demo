e5fe7bc5a2ff5cdb473da3e5055c4cd64a36ae10
ax-vasquez <ar.xa.vasquez@gmail.com>
2021-04-18 14:13:42 -0500

    Endpoint to get user info (#4)

---
+++ src/server/route-handlers/getUser.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import { githubClient } from "../../constants"
+
+export function getUser(
+  req: express.Request,
+  res: express.Response
+) {
+    const {
+        username
+    } = req.query
+
+    try {
+        githubClient.getUser({
+            username: username as string
+        })
+        .then(getUserRes => {
+            res.send(getUserRes.data)
+        })
+    } catch (e) {
+        res.send({
+            error: e.message
+        })
+    }
+    
+}
