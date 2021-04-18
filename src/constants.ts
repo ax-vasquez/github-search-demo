ba2e2822d14b0fd1a17fe34cd7054f09f6cc8be5
ax-vasquez <ar.xa.vasquez@gmail.com>
2021-04-18 11:59:46 -0500

    Base project setup

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+/**
+ * This qualifier must be used with all searches employed by this app; this qualifier
+ * will make it so that the corresponding search will limit the query to users (instead 
+ * of including things like repos, issues, etc.)
+ */
+const USER_QUALIFIER = `type:user`
